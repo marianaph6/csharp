8d19a381653e28589864ea1ca148df9bccb4137c
Mariana Palacios <82735874+marianaph6@users.noreply.github.com>
2023-03-01 21:37:46 -0500

    Implementación del refactor

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -6,15 +6,6 @@ namespace csharp
     [TestFixture]
     public class GildedRoseTest
     {
-        [Test]
-        public void foo()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
-        }
-
 
         [Test]
         public void Quality_Degrades_Twice_As_Fast_Once_SellBy_Date_Passed()
--- Item.cs
+++ Item.cs
@@ -1,4 +1,6 @@
-﻿namespace csharp
+﻿using System;
+
+namespace csharp
 {
     public class Item
     {
@@ -10,5 +12,96 @@
         {
             return this.Name + ", " + this.SellIn + ", " + this.Quality;
         }  
+    }
+
+    public class ItemUpdater
+    {
+        private const int MAX_QUALITY = 50;
+        private const int MIN_QUALITY = 0;
+        private const int SELL_IN_EXPIRED = 0;
+
+        public void UpdateItemQuality(Item item)
+        {
+            if (item.Name == "Sulfuras, Hand of Ragnaros") return;
+
+            UpdateSellIn(item);
+
+            switch (item.Name)
+            {
+                case "Aged Brie":
+                    UpdateAgedBrie(item);
+                    break;
+
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    UpdateBackstagePass(item);
+                    break;
+
+                case "Conjured Mana Cake":
+                    UpdateConjuredItem(item);
+                    break;
+
+                default:
+                    UpdateNormalItem(item);
+                    break;
+            }
+        }
+
+        private void UpdateSellIn(Item item)
+        {
+            item.SellIn--;
+        }
+
+        private void UpdateNormalItem(Item item)
+        {
+            int qualityChange = item.SellIn < SELL_IN_EXPIRED ? 2 : 1;
+            DecreaseQuality(item, qualityChange);
+        }
+
+        private void UpdateAgedBrie(Item item)
+        {
+            IncreaseQuality(item);
+        }
+
+        private void UpdateBackstagePass(Item item)
+        {
+            int qualityChange;
+
+            if (item.SellIn < 0)
+            {
+                item.Quality = MIN_QUALITY;
+                return;
+            }
+
+            if (item.SellIn < 5)
+            {
+                qualityChange = 3;
+            }
+            else if (item.SellIn < 10)
+            {
+                qualityChange = 2;
+            }
+            else
+            {
+                qualityChange = 1;
+            }
+
+            IncreaseQuality(item, qualityChange);
+        }
+
+        private void UpdateConjuredItem(Item item)
+        {
+            int qualityChange = item.SellIn < SELL_IN_EXPIRED ? -4 : -2;
+            DecreaseQuality(item, qualityChange);
+        }
+
+        private void IncreaseQuality(Item item, int amount = 1)
+        {
+            item.Quality = Math.Min(item.Quality + amount, MAX_QUALITY);
+        }
+
+        private void DecreaseQuality(Item item, int amount = 1)
+        {
+            item.Quality = Math.Max(item.Quality - amount, MIN_QUALITY);
+        }
     }
 }
